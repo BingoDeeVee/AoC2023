e9f8fb8703ba72347467a8b9a5665ceec43da391
Bingo DaVinci <mail@bingono.de>
2024-03-24 00:26:37 +0000

    Abstracted logic into function

--- day1/part1.cpp
+++ day1/part1.cpp
@@ -3,39 +3,40 @@
 #include <string>
 using namespace std;
 
-int main() 
-{
+int processLine(string line);
+
+int main() {
     int sum = 0;
     string line;
     ifstream input ("part1_input.txt");
 
-    while(getline(input, line)) 
-    {
-        char first = '\0';
-        char last = '\0';
-        string numberAsString;
-        int number;
-
-        for(int i = 0; i < line.length(); i++)
-        {
-            if(line[i] < 48 || line[i] > 57 )
-            {
-                continue;
-            }
-            if(!first)
-            {
-                first = line[i];
-            }
-            last = line[i];
-        }
-
-        numberAsString += first;
-        numberAsString += last;
-        number = stoi(numberAsString);
-        sum += number;
-        first = '\0';
-        last = '\0';
+    while(getline(input, line)) {
+        sum += processLine(line);
     }
     cout << sum << '\n';
     return 0;
+}
+
+int processLine(string line) {
+    char first = '\0';
+    char last = '\0';
+    string numberAsString = "\0";
+    int number = 0;
+
+    for(int i = 0; i < line.length(); i++) {
+        if(line[i] < 48 || line[i] > 57 )
+        {
+            continue;
+        }
+        if(!first)
+        {
+            first = line[i];
+        }
+        last = line[i];
+    }
+
+    numberAsString = first;
+    numberAsString += last;
+    number = stoi(numberAsString);
+    return number;
 }
